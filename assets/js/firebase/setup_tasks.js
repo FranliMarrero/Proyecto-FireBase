fd8aee85efe875760aea761c6016ae6263e46708
Franli Marrero <franlimarrero17@gmail.com>
2024-02-16 17:42:14 -0500

    Nuevo

---
+++ assets/js/firebase/setup_tasks.js
@@ -0,0 +1,50 @@
+import { createTask, onGetTask, deleteTask } from "./firebase.js";
+
+const taskForm = document.getElementById("create-form");
+const tasksContainer = document.getElementById("tasks-container");
+
+window.addEventListener("DOMContentLoaded", () => {
+    onGetTask((querySnapshot) => {
+        let html = '';
+
+        // READ
+        querySnapshot.forEach(doc => {
+            const data = doc.data();
+
+            html += `
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h4 class="card-title">${data.title}</h4>
+                        <p class="card-text">${data.description}</p>
+                        <div class="row">
+                            <button class='btn btn-danger btn-delete-custom mx-auto col-5' data-id='${doc.id}'>Delete</button>
+                            <button class='btn btn-info btn-edit-custom mx-auto col-5' data-id='${doc.id}'>Edit</button>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+
+        tasksContainer.innerHTML = html;
+
+        // DELETE
+        const btnsDelete = document.querySelectorAll(".btn-delete-custom");
+
+        btnsDelete.forEach(btn => {
+            btn.addEventListener("click", ({target: { dataset }}) => deleteTask(dataset.id));
+        });
+    });
+});
+
+// CREATE
+taskForm.addEventListener("submit", (e) => {
+    // Evitamos que recargue la pagina
+    e.preventDefault();
+
+    const title = taskForm["task-title"].value;
+    const description = taskForm["task-content"].value;
+
+    createTask(title, description);
+
+    taskForm.reset();
+});
